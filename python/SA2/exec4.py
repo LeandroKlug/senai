ea4d3dc4d8494ef963d542b2ea92c0bce75a5220
LeandroKlug <leandro.softnetwork@gmail.com>
2021-08-12 19:30:34 -0300

    sa2 exec4

--- python/SA2/exec4.py
+++ python/SA2/exec4.py
@@ -1,7 +1,3 @@
-#========================================================
-# Crie uma função para desenhar uma linha, usando o caractere '_' (underline). 
-# O tamanho da linha deve ser definido na chamada da função
-
 def linha(n):
  print("_" * n)
 
